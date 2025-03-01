7fa6d1f59317ebb32a263abfca06371ef4028b34
Luiz Davi Vaz <techvazo@outlook.com.br>
2025-02-28 21:08:06 -0300

    First Java Project

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Esse é o Screen Match");
+        System.out.println("Filme: Top Gun: Maverick");
+
+        int anoDeLancamento = 2022;
+        System.out.println("Ano de lançamento: " + anoDeLancamento);
+        boolean incluidoNoPlano = true;
+        double notaDoFilme = 8.1;
+        System.out.println("Nota :"+ notaDoFilme);
+    }
+}
